4fcfc3ea21ee58bed11771cae86a649f6da5e590
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-06-01 23:45:59 +0200

    Add application JS file for Backbone stuff.

---
+++ public/js/jobupapp.js
@@ -0,0 +1,23 @@
+//____________________________________________________________________
+// File: jobupapp.js
+//____________________________________________________________________
+//
+// Author: Shaun Ashby <shaun@ashby.ch>
+// Created: 2014-06-01 22:01:13+0200
+// Revision: $Id$
+// Description: The main Javascript file for loading the Backbone app environment.
+//
+// Copyright (C) 2014 Shaun Ashby
+//
+//
+//--------------------------------------------------------------------
+
+$(function(){
+     // Models:
+     var JobSearch = Backbone.Model.extend({
+     });
+      // Views:
+
+      // Collections:
+
+}).call();
